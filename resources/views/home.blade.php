b120c4f2c943e9ed56d66e33be67a4c12df0c2f4
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-12-07 15:59:24 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    <div>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="/shop">shop</a></li>
+                    <li><a href="/aboutUs">about us</a></li>
+                    <li><a href="/contact">contact</a></li>
+                    <li><a href="/login">login</a></li>
+                </ul>
+            </nav>
+            <h1>Home page</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
